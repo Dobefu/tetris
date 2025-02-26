3fb9cf0b22e746997105ee0dc893e6549ac5fc3e
Connor van Spronssen <info@dobefu.com>
2025-02-26 17:44:17 +0100

    feat: Add a background plane

--- app.vue
+++ app.vue
@@ -1,13 +1,27 @@
-<template>
-  <TresCanvas clear-color="#000" preset="realistic" window-size>
-    <TresPerspectiveCamera />
+<script setup lang="ts"></script>
 
-    <TresMesh>
+<template>
+  <TresCanvas clear-color="#000" preset="realistic" shadows window-size>
+    <TresPerspectiveCamera :position="[0, 0, 50]" :look-at="[0, 0, 0]" />
+
+    <TresMesh cast-shadow :position="[0, 0, 0]">
       <TresBoxGeometry />
       <TresMeshPhongMaterial color="gray" />
     </TresMesh>
 
-    <TresDirectionalLight v-light-helper />
+    <TresMesh receive-shadow :scale="[10, 20, 1]" :position="[0, 0, -1]">
+      <TresPlaneGeometry />
+      <TresMeshPhongMaterial color="gray" />
+    </TresMesh>
+
+    <OrbitControls />
+
+    <TresDirectionalLight
+      cast-shadow
+      :position="[-5, 10, 10]"
+      :look-at="[0, 0, 0]"
+      :intensity="1"
+    />
 
     <TresAmbientLight :intensity="1" />
   </TresCanvas>
