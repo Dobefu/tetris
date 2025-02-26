b1625c6e4dcc6882ccda347a208c4a1b51aadbe7
Connor van Spronssen <info@dobefu.com>
2025-02-26 08:50:08 +0100

    feat: Add a basic box mesh to the scene

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <TresCanvas window-size>
+  <TresCanvas clear-color="#000" preset="realistic" window-size>
     <TresPerspectiveCamera />
+
+    <TresMesh>
+      <TresBoxGeometry />
+      <TresMeshPhongMaterial color="gray" />
+    </TresMesh>
+
+    <TresDirectionalLight v-light-helper />
+
+    <TresAmbientLight :intensity="1" />
   </TresCanvas>
 </template>
