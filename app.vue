1c8acc9ba4024a4e5c88ad3ddeaf049424066995
Connor van Spronssen <info@dobefu.com>
2025-02-27 18:22:46 +0100

    feat Move the game scene to a separate component

--- app.vue
+++ app.vue
@@ -1,20 +1,5 @@
 <template>
   <TresCanvas clear-color="#000" shadows window-size render-mode="on-demand">
-    <TresPerspectiveCamera :position="[0, 0, 50]" :look-at="[0, 0, 0]" />
-
-    <TetriminoBlock :position="[0, 0, 0]" />
-
-    <Board />
-
-    <OrbitControls />
-
-    <TresDirectionalLight
-      cast-shadow
-      :position="[-5, 10, 10]"
-      :look-at="[0, 0, 0]"
-      :intensity="1"
-    />
-
-    <TresAmbientLight :intensity="1" />
+    <ScenesGame />
   </TresCanvas>
 </template>
---
+++ components/scenes/game.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <TresPerspectiveCamera :position="[0, 0, 50]" :look-at="[0, 0, 0]" />
+  <OrbitControls />
+
+  <TresAmbientLight :intensity="1" />
+  <TresDirectionalLight
+    cast-shadow
+    :position="[-5, 10, 10]"
+    :look-at="[0, 0, 0]"
+    :intensity="1"
+  />
+
+  <Board />
+  <TetriminoBlock :position="[0, 0, 0]" />
+</template>
