65c1965c29eba94e51872d425ead27e455e91ae8
Connor van Spronssen <info@dobefu.com>
2025-02-27 18:10:11 +0100

    feat: Add ESLint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@tresjs/nuxt'],
+  modules: ['@tresjs/nuxt', '@nuxt/eslint'],
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
 })
