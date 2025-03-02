e6e179751757bac9cc0dde3c90b6820b5d029dfb
Connor van Spronssen <info@dobefu.com>
2025-03-02 13:12:26 +0100

    feat: Add an object to hold the board grid

--- components/scenes/game.vue
+++ components/scenes/game.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const boardCells = reactive(
+  Array.from({ length: 40 }, () => Array.from({ length: 10 }, () => null)),
+)
+</script>
 
 <template>
   <TresPerspectiveCamera :position="[0, 0, 50]" :look-at="[0, 0, 0]" />
@@ -19,10 +23,4 @@
 
   <Board />
   <Tetromino :position="[0, 4.5, 0]" type="I" />
-  <Tetromino :position="[0, 3.5, 0]" type="O" />
-  <Tetromino :position="[0, 1.5, 0]" type="T" />
-  <Tetromino :position="[0, -0.5, 0]" type="J" />
-  <Tetromino :position="[0, -3.5, 0]" type="L" />
-  <Tetromino :position="[0, -6.5, 0]" type="S" />
-  <Tetromino :position="[0, -8.5, 0]" type="Z" />
 </template>
